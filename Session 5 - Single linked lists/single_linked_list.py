2e45120b00da08ef62ebcefd80d948ea6e248e28
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-09-30 16:06:15 +0100

    Add Node class for single linked list
    Introduced a Node class with value and next reference for single linked list operations. Updated main to demonstrate linked list creation with a head node.

--- Session 5 - Single linked lists/single_linked_list.py
+++ Session 5 - Single linked lists/single_linked_list.py
@@ -1,18 +1,25 @@
 # Node class
+# We'll use it in several instances, so worth having
+# Node for single linked list
 
-# Creation
+# Node class
+# With value and reference to next element
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
-# Adding
-
-# Traversal
-
+# Traversal/print
+# Appending
 # Insertion
-
 # Deletion
 
 def main():
-    pass
+    # Linked list creation = create the new node
+    # The first node IS the list
+    head = Node(10)
+    print()
+
 
 if __name__ == "__main__":
     main()
-    

49311e840b2d949a59f4128ced3c8bd1baaa70cc
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-10-07 15:29:37 +0100

    Add stack implementation using list functions
    Introduces basic stack operations (push, pop, peek, isEmpty) using Python lists and demonstrates their usage in a main function.

---
+++ Session 6 - Stacks and Queues/stack_array_w3s.py
@@ -0,0 +1,36 @@
+# Some stack functions (non-OO)
+
+def push(s, new_element):
+    s.append(new_element)
+    
+def pop(s):
+    return s.pop()
+
+def peek(s):
+    return s[-1]
+
+def isEmpty(s):
+    return len(s) == 0
+
+def main():
+    # The stack is a list, but we just use the functions above
+    stack = []
+
+    # Push
+    push(stack, 'A')
+    push(stack, 'B')
+    push(stack, 'C')
+    print("Stack: ", stack)
+    # Pop
+    element = pop(stack)
+    print("Pop: ", element)
+    # Peek
+    topElement = peek(stack)
+    print("Peek: ", topElement)
+    # isEmpty
+    print("isEmpty: ", isEmpty(stack))
+    # Size
+    print("Size: ",len(stack))
+
+if __name__ == "__main__":
+    main()

68da9f186d795aecdb23dbd1ddf28e21ef6d6d2e
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-10-07 16:43:23 +0100

    Implement dequeue and peek for linked list queue
    Added dequeue and peek functions to the queue implementation using a linked list. Updated main to demonstrate enqueue and dequeue operations.

--- Session 6 - Stacks and Queues/queue_ll.py
+++ Session 6 - Stacks and Queues/queue_ll.py
@@ -9,18 +9,53 @@ class Node:
 def enqueue(q, value):
     # if the the queue is empty
     n = Node(value)
-    
+    # Whether the queue is empty (none) or not, just set it as next
+    n.next = q
+    q = n
+    return q
 
 # Dequeue
+def dequeue(q):
+    head = q
+    previous = None
+    # Traverse the linked list
+    while q.next:
+        previous = q
+        q = q.next
+
+    # Save the value
+    v = q.value
+    # Delete the node
+    previous.next = None
+    # Return the value
+    return v, head
 
 # Peek
+def peek(q):
+    # Traverse the linked list
+    while q.next:
+        q = q.next
+
+    return q.value
 
 # isEmpty
 
 # size
 
 def main():
-    pass
+    # Reserves the name for the variable but assign nothing
+    # Enqueue will do the job
+    queue = None
+
+    for p in ["Erik", "Jack", "Kadie", "Grant"]:
+        queue = enqueue(queue, p)
+
+    v, queue = dequeue(queue)
+    print(v)
+    v, queue = dequeue(queue)
+    print(v)
+
+    print()
 
 if __name__ == "__main__":
     main()

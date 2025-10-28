c11fbbc4b9ee475ba0eabdc8be6cc6306ad5fc28
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-10-28 16:34:15 +0000

    Implement enqueue and dequeue methods for Queue class
    Added enqueue and dequeue methods to the Queue class using a linked list approach. Also included test code to demonstrate queue operations. Commented out previous test code for QueueA to focus on the new Queue implementation.

--- Session 6 - Stacks and Queues/queue_oo.py
+++ Session 6 - Stacks and Queues/queue_oo.py
@@ -24,7 +24,58 @@ class Queue:
     def isEmpty(self):
         return self.front is None
     
-    
+    # Add element
+    def enqueue(self, new_data):
+        # Create a new node
+        new_node = Node(new_data)
+        # If list is empty set back and front to node
+        if self.isEmpty():
+            self.front = self.back = new_node
+        else:
+        # Otherwise set the back's next to the new node
+        # And the new node to the new back
+            self.back.next = new_node
+            self.back = new_node
+
+        # Update size
+        self.current_size += 1
+
+    # Remove element from queue
+    # If empty return None
+    # Otherwise, return front element, after severing link
+    def dequeue(self):
+        if self.isEmpty():
+            return None
+        else:
+            # Save the returned data to a variable
+            returned_data = self.front.data
+            # Set New front to next element
+            self.front = self.front.next
+            # If the new front is "None"
+            if self.front is None:
+                self.back = None
+            self.current_size -= 1
+            return returned_data
+
+
+# Create a queue and add a few elements
+q = Queue()
+
+q.enqueue("Hi")
+q.enqueue("Bonjour")
+q.enqueue("Hola")
+q.enqueue("Guten Tag")
+
+print(q.dequeue())
+print(q.dequeue())
+
+
+
+
+
+
+
+
 
 
 
@@ -48,14 +99,14 @@ class QueueA:     # A for array
 # Just some test code
 # Create a new queue
 # Object name is q1, class is queueA
-q1 = QueueA()
+# q1 = QueueA()
 
-# Insert some data
-q1.enqueue("test")
-q1.enqueue("another test")
-q1.enqueue("something")
+# # Insert some data
+# q1.enqueue("test")
+# q1.enqueue("another test")
+# q1.enqueue("something")
 
-print(q1.array)
+# print(q1.array)
 
-print(q1.dequeue())
-print(q1.dequeue())
+# print(q1.dequeue())
+# print(q1.dequeue())

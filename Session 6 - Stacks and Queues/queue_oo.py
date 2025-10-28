382ad6871f4720203d5c23c294016362d6079b67
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-10-28 15:24:46 +0000

    Add initial object-oriented queue implementation
    Introduces a basic queue class (queueA) using an array as the underlying data structure. Includes constructor and placeholder for queue methods.

---
+++ Session 6 - Stacks and Queues/queue_oo.py
@@ -0,0 +1,10 @@
+# Queue in Python - object oriented
+# Based on array/linked list
+
+# Quick example - with an array
+
+class queueA:     # A for array
+    def __init__(self):     # Initialiser / constructor
+        self.array = []   # List backing the queue
+    
+    # Methods to add data and dequeue data

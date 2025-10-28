4269c516d054c1bff405b02169589fee5bb3301c
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-10-28 15:31:35 +0000

    Add enqueue and dequeue methods to queueA class
    Implemented enqueue and dequeue methods for the queueA class using a list as the backing store. Enqueue inserts at index 0, and dequeue pops from the end, returning None if the queue is empty.

--- Session 6 - Stacks and Queues/queue_oo.py
+++ Session 6 - Stacks and Queues/queue_oo.py
@@ -8,3 +8,15 @@ class queueA:     # A for array
         self.array = []   # List backing the queue
     
     # Methods to add data and dequeue data
+    def enqueue(self, new_data):
+        # Just a proxy to inserting data at index 0
+        self.array.insert(0, new_data)
+
+    def dequeue(self):
+        # if list is empty return None
+        if len(self.array) == 0:
+            return None
+        else:
+            return self.array.pop()
+ 
+# Just some test code

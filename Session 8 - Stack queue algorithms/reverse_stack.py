9092eb6fba08ae878140f610384bd4e25efa73dc
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-11-04 15:59:06 +0000

    Implement stack inversion with debug output
    Added logic to invert a stack by popping elements from the original and pushing them to a new stack. Included debug print statements to trace the process and updated the main function to demonstrate inversion and output the final stack contents.

--- Session 8 - Stack queue algorithms/reverse_stack.py
+++ Session 8 - Stack queue algorithms/reverse_stack.py
@@ -17,7 +17,15 @@ def invert_stack(some_stack):
     # Create a new stack
     new_stack = stack_aa.Stack()
     
-    # 
+    # while stack not empty
+    # pop element and push to other stack
+    while not some_stack.isEmpty():
+        e = some_stack.pop()
+        print(e, "popped from stack 1 and pushed to stack 2")
+        new_stack.push(e)
+        # For debug
+    
+    return new_stack
 
 
 def main():
@@ -28,8 +36,15 @@ def main():
     # Insert all the sample data to the stack
     for e in sample_data:
         s.push(e)
+        print(e, "pushed to stack 1")
     
-    print(s)
+    # Test the inversion
+    s = invert_stack(s)
+
+    print("Final stack 2")
+    while not s.isEmpty():
+        print(s.pop())
+    
     pass
 
 if __name__ == "__main__":

3b2004699fb1ce499128499d94930e25f7839168
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-11-18 16:00:17 +0000

    Add tree traversal functions and usage examples
    Implemented in_order, pre_order, and post_order traversal functions for binary trees. Added example usage to demonstrate traversals on a sample tree.

--- Binary Trees/binary_tree.py
+++ Binary Trees/binary_tree.py
@@ -7,6 +7,29 @@ class BTreeNode:
         self.left = None
         self.right = None
 
+# in order: left, node, right
+def in_order(node):
+    # if node not empty
+    if node:
+        in_order(node.left)
+        print(node.value, end=" ")
+        in_order(node.right)
+    # Recursion: no need to worry about loops
+
+def pre_order(node):
+    if node:
+        print(node.value, end=" ")
+        pre_order(node.left)
+        pre_order(node.right)
+
+def post_order(node):
+    if node:
+        post_order(node.left)
+        post_order(node.right)
+        print(node.value, end=" ")
+
+
+
 root = BTreeNode('R')
 nodeA = BTreeNode('A')
 nodeB = BTreeNode('B')
@@ -26,4 +49,11 @@ nodeB.left = nodeE
 nodeB.right = nodeF
 
 nodeF.left = nodeG
+
+in_order(root)
+print()
+pre_order(root)
+print()
+post_order(root)
+print()
 
